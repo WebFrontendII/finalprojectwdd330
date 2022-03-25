fcfcd001006e8f52a32b8c8b57773e762c34e002
treysonsearle <treyson.searle96@gmail.com>
2022-03-24 18:31:17 -0700

    env stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
+import API_KEY from "./API_KEY"
 
-
+console.log(API_KEY)
 
 const searchMovies = async (searchPhrase) => {
-   fetch(`http://www.omdbapi.com/?s=${searchPhrase}&apikey=28faa7ff`)
+   fetch(`http://www.omdbapi.com/?s=${searchPhrase}&apikey=${API_KEY}`)
       .then(response => response.json())
       .then(data => {
          console.log(data)
-         console.log(process.env)
       })
 }
 
